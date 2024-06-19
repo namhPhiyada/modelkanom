e238ae3d0baddd00370a3aa1a175c8932a2ebac1
namhPhiyada <109198499+namhPhiyada@users.noreply.github.com>
2024-06-19 19:26:27 +0700

    Update app.py

--- app.py
+++ app.py
@@ -18,30 +18,36 @@ model = torch.hub.load('ultralytics/yolov5', 'custom', path='models/Model2.pt')
 
 uploaded_file = st.file_uploader("Choose .jpg pic ...", type="jpg")
 if uploaded_file is not None:
-
     file_bytes = np.asarray(bytearray(uploaded_file.read()))
     image = cv2.imdecode(file_bytes, 1)
 
-    imgRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    # st.image(imgRGB)
+    imgRGB = cv2.cvtColor(image , cv2.COLOR_BGR2RGB)
 
     st.write("")
     st.write("Detecting...")
     result = model(imgRGB, size=600)
-
+  
     detect_class = result.pandas().xyxy[0]
+  
+    st.code(detect_class[['name', 'xmin','ymin', 'xmax', 'ymax']])
+  
+    # Count each class
+    class_counts = detect_class['name'].value_counts().to_dict()
+    st.write("Class counts:")
+    
+    for class_name, count in class_counts.items():
+        st.write(f"- **{class_name.capitalize()}**: {count}")
 
-    detect_class['name'] = detect_class['name'].map({'Darathong': 'Darathong',
-                                                     'SaneCharn': 'SaneCharn',
-                                                     'ChorMuang': 'ChorMuang'})
-
-    # Get unique names
-    unique_names = detect_class['name'].unique()
-
-    # Display the unique names without numbers
-    st.write("")
-    for name in unique_names:
-         st.write(name)
+    
+  
+    outputpath = 'output.jpg'
+  
+    result.render()  # render bbox in image
+    for im in result.ims:
+        im_base64 = Image.fromarray(im)
+        im_base64.save(outputpath)
+        img_ = Image.open(outputpath)
+        st.image(img_, caption='Model Prediction(s)')
     # for name in unique_names:
     #     if name == 'Darathong':
     #         st.text('TH: ดาราทอง  EN: Darathong')
